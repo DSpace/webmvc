a226dfa8ccc18027ad1ec709a74db12334235c67
Graham Triggs <grahamtriggs@gmail.com>
2010-11-18 00:17:29 +0000

    FreemarkerUI - simple parent theme support
    git-svn-id: http://scm.dspace.org/svn/repo/modules/webmvc/trunk@5876 9c30dcfa-912a-0410-8fc2-9e0234be79fd

---
+++ webmvc-api/src/main/java/org/dspace/webmvc/theme/SpringThemeUtils.java
@@ -0,0 +1,20 @@
+package org.dspace.webmvc.theme;
+
+import org.springframework.context.NoSuchMessageException;
+import org.springframework.ui.context.Theme;
+
+import java.util.Locale;
+
+public class SpringThemeUtils {
+    public static String getProperty(Theme theme, String property, Locale locale) {
+        return theme == null ? null : theme.getMessageSource().getMessage(property, null, locale);
+    }
+
+    public static String getProperty(Theme theme, String property, Locale locale, String defaultValue) {
+        try {
+            return theme == null ? null : theme.getMessageSource().getMessage(property, null, locale);
+        } catch (NoSuchMessageException nsme) {
+            return defaultValue;
+        }
+    }
+}
