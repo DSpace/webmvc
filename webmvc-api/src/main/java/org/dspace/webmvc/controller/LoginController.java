ba4dd8fb0150bb4c7d61a08c95f26e938ff37eb3
grahamtriggs <grahamtriggs@9c30dcfa-912a-0410-8fc2-9e0234be79fd>
2011-02-28 17:17:33 +0000

    Freemarker - Controller handling, login form
    git-svn-id: http://scm.dspace.org/svn/repo/modules/webmvc/trunk@6100 9c30dcfa-912a-0410-8fc2-9e0234be79fd

--- webmvc-api/src/main/java/org/dspace/webmvc/controller/LoginController.java
+++ webmvc-api/src/main/java/org/dspace/webmvc/controller/LoginController.java
@@ -11,21 +11,84 @@
 
 package org.dspace.webmvc.controller;
 
+import com.sun.deploy.net.HttpRequest;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 //@RequestMapping("/login")
 public class LoginController {
+    @ModelAttribute("loginForm")
+    public LoginForm createForm(
+                @RequestParam(value="login_email",    required=false) String email,
+                @RequestParam(value="login_password", required=false) String password,
+                @RequestParam(value="login_url",      required=false) String redirectUrl
+                ) {
+        LoginForm loginForm = new LoginForm();
+
+        loginForm.setEmail(email);
+        loginForm.setPassword(password);
+        loginForm.setUrl(redirectUrl);
+
+        return loginForm;
+    }
 
     @RequestMapping
-    public String showForm() {
+    public String showForm(@ModelAttribute("loginForm") LoginForm loginForm, HttpServletRequest request) {
+        if (StringUtils.isEmpty(loginForm.getUrl())) {
+            String referer = request.getHeader("referer");
+
+            if (!StringUtils.isEmpty(referer) && !referer.contains("/login")) {
+                loginForm.setUrl(referer);
+            }
+        }
+
         return "pages/login";
     }
 
     @RequestMapping(params = "submit")
-    public String processForm() {
-        return null;
+    public String processForm(@ModelAttribute("loginForm") LoginForm loginForm) {
+
+        if (!StringUtils.isEmpty(loginForm.getUrl())) {
+            return "redirect:" + loginForm.getUrl();
+        }
+
+        return "pages/login";
+    }
+
+    public class LoginForm {
+        private String email;
+        private String password;
+        private String url;
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
     }
 }
