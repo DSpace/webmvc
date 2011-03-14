1a9685af92d6dfca0edfda78bbd52f40413760e2
Graham Triggs <grahamtriggs@gmail.com>
2011-03-14 16:53:57 +0000

    Freemarker - License editor
    git-svn-id: http://scm.dspace.org/svn/repo/modules/webmvc/trunk@6135 9c30dcfa-912a-0410-8fc2-9e0234be79fd

--- freemarkerui-webapp/src/main/webapp/pages/admin/license-saved.ftl
+++ freemarkerui-webapp/src/main/webapp/pages/admin/license-saved.ftl
@@ -16,13 +16,7 @@
     <body>
         <h1 class="ds-div-head"><@dspace.message "ui.admin.license.edit.heading" /></h1>
         <div class="ds-static-div primary">
-            <p class="ds-paragraph"><@dspace.message "ui.admin.license.edit.description" /></p>
-            <form id="LicenseEdit_div_general-query" class="ds-interactive-div secondary licenseEdit" action="" method="post">
-                <textarea rows="20" cols="80" name="licenseText">${licenseForm.licenseText?html}</textarea>
-                <p class="ds-paragraph button-list">
-                    <input id="LicenseEdit_field_submit" class="ds-button-field" name="submit" type="submit" value="<@dspace.message "ui.form.controls.save" />">
-                </p>
-            </form>
+            <p class="ds-paragraph"><@dspace.message "ui.admin.license.edit.saved" /></p>
         </div>
     </body>
 </html>
--- webmvc-api/src/main/java/org/dspace/webmvc/controller/LicenseEditController.java
+++ webmvc-api/src/main/java/org/dspace/webmvc/controller/LicenseEditController.java
@@ -1,16 +1,25 @@
 package org.dspace.webmvc.controller;
 
 import org.apache.commons.lang.StringUtils;
+import org.dspace.core.ConfigurationManager;
+import org.dspace.core.Context;
+import org.dspace.core.I18nUtil;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
+
 @Controller
 public class LicenseEditController {
     @ModelAttribute("licenseForm")
-    public LicenseForm createForm() {
-        return new LicenseForm();
+    public LicenseForm createForm(Context context) {
+        LicenseForm licenseForm = new LicenseForm();
+        licenseForm.setLicenseText(ConfigurationManager.getLicenseText(I18nUtil.getDefaultLicense(context)));
+        return licenseForm;
     }
 
     @RequestMapping
@@ -19,12 +28,18 @@ public class LicenseEditController {
     }
 
     @RequestMapping(params = "submit")
-    public String processForm(LicenseForm licenseForm) {
-        return showForm(licenseForm);
+    public String processForm(Context context, @Valid LicenseForm licenseForm, BindingResult bindingResult) {
+        if (!bindingResult.hasErrors()) {
+            ConfigurationManager.writeLicenseFile(I18nUtil.getDefaultLicense(context), licenseForm.getLicenseText());
+            return "pages/admin/license-saved";
+        }
+
+        return "pages/admin/license";
     }
 
 
     public static class LicenseForm {
+        @NotEmpty
         private String licenseText;
 
         public String getLicenseText() {
