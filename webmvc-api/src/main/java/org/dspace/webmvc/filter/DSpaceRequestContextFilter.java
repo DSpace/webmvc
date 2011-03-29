286a73089fc2bdf5544f711597d6cb32ee3a5377
Graham Triggs <grahamtriggs@gmail.com>
2011-03-29 15:58:21 +0000

    Freemarker - Authenctication updates
    git-svn-id: http://scm.dspace.org/svn/repo/modules/webmvc/trunk@6279 9c30dcfa-912a-0410-8fc2-9e0234be79fd

--- webmvc-api/src/main/java/org/dspace/webmvc/filter/DSpaceRequestContextFilter.java
+++ webmvc-api/src/main/java/org/dspace/webmvc/filter/DSpaceRequestContextFilter.java
@@ -1,6 +1,10 @@
 package org.dspace.webmvc.filter;
 
+import org.apache.log4j.Logger;
+import org.dspace.constants.Constants;
 import org.dspace.core.Context;
+import org.dspace.eperson.EPerson;
+import org.dspace.webmvc.model.login.HttpLoginService;
 import org.dspace.webmvc.utils.DSpaceRequestUtils;
 
 import javax.servlet.Filter;
@@ -9,18 +13,43 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.SQLException;
 
 public class DSpaceRequestContextFilter implements Filter {
+    private static final Logger log = Logger.getLogger(DSpaceRequestContextFilter.class);
 
     public void init(FilterConfig filterConfig) throws ServletException {
     }
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        //Set encoding to UTF-8, if not set yet
+        //This avoids problems of using the HttpServletRequest
+        //in the getSpecialGroups() for an AuthenticationMethod,
+        //which causes the HttpServletRequest to default to
+        //non-UTF-8 encoding.
+        try {
+            if(request.getCharacterEncoding()==null) {
+                request.setCharacterEncoding(Constants.DEFAULT_ENCODING);
+            }
+        } catch(Exception e) {
+            log.error("Unable to set encoding to UTF-8.", e);
+        }
+
         Context context = null;
         try {
             context = new Context();
+
+            if (request instanceof HttpServletRequest) {
+                HttpLoginService hls = new HttpLoginService((HttpServletRequest)request);
+                EPerson person = hls.getUser(context);
+                if (person != null) {
+                    context.setCurrentUser(person);
+                }
+            }
+
             DSpaceRequestUtils.setDSpaceContext(request, context);
             chain.doFilter(request, response);
         } catch (SQLException e) {
